2bcdb3ecb56fa43df96ba454f1be4377c3ebc0ee
Pedro Henrique <158114330+PdroOlive@users.noreply.github.com>
2025-05-26 18:11:19 -0300

    Atualizei o Home e adicionei um arquivo do modal
    Modal ainda não está funcionando!

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,20 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import Ionicons from '@expo/vector-icons/Ionicons';
+import Clients from './Clients'
 
 export default function Home() {
     return(
         <View style={styles.container}>
-            <Text style={styles.heanding}>Agroçougue</Text>
-            <Text>CNPJ</Text>
-            <Text>Horário de Funcionamento</Text>
-            <Text>Cobrança:</Text>
-
+            <View>
+                <Text style={styles.heanding}>Agroçougue</Text>
+                <Text style={styles.paragraph}>06.207.421.0010-65</Text>
+                <Text style={styles.paragraph}>Seg a Sex: 08 hrs as 18:00</Text>
+                <Text style={styles.paragraph}>Inadimplente</Text>
+            </View>
+            <TouchableOpacity>
+                <Ionicons name="add-circle"  size={32} />
+            </TouchableOpacity>
+            <Clients />
         </View>
     )
 }
@@ -15,11 +22,23 @@ export default function Home() {
 const styles = StyleSheet.create({
     container: {
         display: "flex",
-      
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        paddingInline: 20,
+        paddingBlock: 10,
+        borderBottomWidth: 1
+
     },
     heanding: {
         fontFamily: "sans-serif",
-        fontSize: 20,
+        fontSize: 25,
         fontWeight: 700,
+    },
+    paragraph: {
+        fontSize: 15,
+        fontWeight: 700,
+        fontFamily: "sans-serif",
+
     }
 })
